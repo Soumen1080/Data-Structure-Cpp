c5ab4bdd65d339a51a89f4f530e958b0e88f9ba2
Soumen Mandal <soumenmandal1080@gmail.com>
2026-03-13 13:45:33 +0530

    Remove helloWorld example and add command line argument handling and selection sort implementations

---
+++ sorting algorithams/selection sort.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    cout<< "Enter number of elements:";
+    cin >> n ;
+    int arr[n] ;
+    cin >> arr[n];
+ selection_sort(arr[n],n);
+
+}
+void selection_sort(int arr[] , int n ){
+
+
+}
